344e0e48f608b8b945720a774f8189dc2beb91fa
ebrivera <ebrivera@bu.edu>
2025-02-21 02:11:41 -0500

    model

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
-    '''Encapsulate the idea of an Article by some author.'''
+    '''Encapsulate the idea of a Profile with personal information.'''
 
-    # define the data attributes of the Article object
-    title = models.TextField(blank=True)
-    author = models.TextField(blank=True)
-    text = models.TextField(blank=True)
-    published = models.DateTimeField(auto_now=True)
-    image_url = models.URLField(blank=True)
+    # define the data attributes of the Profile object
+    first_name = models.TextField(blank=True)
+    last_name = models.TextField(blank=True)
+    city = models.TextField(blank=True)
+    email = models.EmailField(blank=True)
+    profile_image_url = models.URLField(blank=True)
+    
     
 
     def __str__(self):
